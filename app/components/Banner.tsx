00b6617bdf232d1729710182a9fa3a097031b801
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-12-06 17:42:22 +0000

    00001

---
+++ app/components/Banner.tsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+    return (
+        <div className="flex">
+            odsndosnfsdonf
+            <h2>Digite /admin na URL acima</h2>
+
+        </div>
+    )
+
+
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="p-12 flex justify-between items-center border-b">
+            <h4>Universidade</h4>
+            <nav></nav>
+        </header>
+    )
+}
