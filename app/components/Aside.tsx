7158523c9b13b2186f18dfba9372744c86f5b5f5
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-12-06 19:30:57 +0000

    0004

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -1,9 +1,9 @@
 export default function Aside() {
     return (
         <div className="w-1/6 text-black  py-8 px-6 ">
-            <a className="py-16  px-3"  href="/admin/course">Listar</a>
+            <a className="py-16  px-3"  href="/admin/register">Listar</a>
             <br></br>
-            <a className="py-16 px-3"  href="/admin/course/new">Cadastrar</a>
+            <a className="py-16 px-3"  href="/admin/register/save">Cadastrar</a>
             <br></br>
             <a className="py-16 px-3 " href="/admin/course/new">Deletar</a>
 
